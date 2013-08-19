14a393f2811165dd87560e70869d9a59584d13bc
igorcb <igor.batista@gmail.com>
2013-08-19 12:12:21 -0300

    config imagemagik and send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Sucata::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  Paperclip.options[:command_path] = "/usr/bin/"
+
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = {:address => "smtp.gmail.com", 
+                                        :port                 => 587, 
+                                        :domain               => 'gmail.com', 
+                                        :user_name            => 'igorcb', 
+                                        :password             => 'syncmaster550v', 
+                                        :authentication       => 'plain', 
+                                        :enable_starttls_auto => true  }
 end
